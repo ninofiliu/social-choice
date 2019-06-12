6171d70d8ef740661a843bde147b9ee803dc710b
ninofiliu <nino.filiu@telecom-paristech.fr>
2019-06-12 11:57:47 +0200

    F : Profile : constructor and basic methods

--- src/profile.js
+++ src/profile.js
@@ -3,15 +3,28 @@ export default class Profile {
     /**
      * Creates a rich preference profile object with initialized values of:
      * - condorcet winner
-     * - C1 graph
-     * - C2 graph
-     * @param {{nb: number, ranking: number[]}[]} r 
+     * - p-graph aka C1-graph: oriented graph of pairwise comparisons
+     * - n-graph aka C2-graph: graph of n_xy values
+     * 
+     * The r parameter defines the R_N object as such:
+     * - `r[i].nb`: number of voters who have the preference R_i defined by `r[i].ranks`
+     * - `r[i].ranks[j]`: rank of the j-th alternative when ordered as defined by the relation R_i
+     * @param {{nb: number, ranks: number[]}[]} r 
      */
     constructor(r) {
         this.r = r;
-        this.n = null; // TODO
-        this.p = null; // TODO
-        this.cw = null; // TODO
+        this.n = r.reduce((acc, cur) => acc+cur.nb, 0);
+        this.m = r[0].ranks.length;
+        this.nGraph = (new Array(this.m)).fill(null).map((_,x) => (
+            (new Array(this.m)).fill(null).map((_,y) => (
+                x==y ? null : this.getNXY(x,y)
+            ))
+        ));
+        this.pGraph = (new Array(this.m)).fill(null).map((_,x) => (
+            (new Array(this.m)).fill(null).map((_,y) => (
+                x==y ? null : this.nGraph[x][y]>this.nGraph[y][x]
+            ))
+        ));
     }
 
     /**
@@ -20,9 +33,14 @@ export default class Profile {
      * @param {number} y
      * @returns {number} 
      */
-    getN(x,y) {
-        // TODO
-        return null;
+    getNXY(x,y) {
+        let ret = 0;
+        for (let ri of this.r) {
+            if (ri.ranks[x]<ri.ranks[y]) {
+                ret+= ri.nb;
+            }
+        }
+        return ret;
     }
 
     /**
@@ -31,7 +49,14 @@ export default class Profile {
      * @returns {number|null}
      */
     getCW() {
-        // TODO
-        return null;
+        for (let x=0; x<this.m; x++) {
+            let winAll = true;
+            for (let y=0; y<this.m; y++) {
+                if (x!=y) {
+                    winAll = winAll && this.pGraph[x][y];
+                }
+            }
+            if (winAll) return x;
+        }
     }
 }
--- test/index.js
+++ test/index.js
@@ -1,3 +1,38 @@
-describe('Dummy test', () => {
-    it('Should work', () => {})
-})
+const assert = require('assert');
+const {
+    Profile,
+    scf: {
+        plurality
+    }
+} = require('../dist/main');
+
+describe('Profile', () => {
+    let profile;
+    it('Can construct a profile', () => {
+        profile = new Profile([
+            { nb: 1, ranks: [0, 1, 2] },
+            { nb: 1, ranks: [1, 2, 0] },
+            { nb: 1, ranks: [2, 0, 1] },
+        ]);
+    });
+    it('Can build its n-graph', () => {
+        assert.deepEqual(
+            profile.nGraph,
+            [
+                [ null, 2, 1 ],
+                [ 1, null, 2 ],
+                [ 2, 1, null ]
+            ]
+        );
+    });
+    it('Can build its p-graph', () => {
+        assert.deepEqual(
+            profile.pGraph,
+            [
+                [ null, true, false ],
+                [ false, null, true ],
+                [ true, false, null ]
+            ]
+        );
+    });
+});
