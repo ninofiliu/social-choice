3508b5ef41272b66899d976f1e34c71476d5bcbc
ninofiliu <nino.filiu@telecom-paristech.fr>
2019-06-18 12:49:38 +0200

    D : tests : add mocha types

--- test/index.js
+++ test/index.js
@@ -1,15 +1,10 @@
 const assert = require('assert');
-const {
-    Profile,
-    scf: {
-        plurality
-    }
-} = require('../dist/main');
+const sc = require('..');
 
 describe('Profile', () => {
     let profile;
     it('Can construct a profile', () => {
-        profile = new Profile([
+        profile = new sc.Profile([
             { nb: 1, ranks: [0, 1, 2] },
             { nb: 1, ranks: [1, 2, 0] },
             { nb: 1, ranks: [2, 0, 1] },
@@ -42,11 +37,11 @@ describe('Profile', () => {
 
 describe('SCF', () => {
     it('Plurality', () => {
-        const profile = new Profile([
+        const profile = new sc.Profile([
             { nb: 1, ranks: [0, 1, 2] },
             { nb: 1, ranks: [1, 2, 0] },
             { nb: 1, ranks: [2, 0, 1] },
         ]);
-        assert.deepEqual(plurality(profile), [0,1,2]);
+        assert.deepEqual(sc.scf.plurality(profile), [0,1,2]);
     });
 });
